a19fb7758f10bbc734130adcda86d670ccf27358
Lahiru075 <sanjeewalahiru057@gmail.com>
2026-03-11 10:07:40 +0530

    fix error

--- sentura_test/src/main/java/lk/ijse/gdse/countryservice/dto/CountryDTO.java
+++ sentura_test/src/main/java/lk/ijse/gdse/countryservice/dto/CountryDTO.java
@@ -9,4 +9,29 @@ public class CountryDTO {
     private String region;
     private long population;
     private String flag;
+
+    public CountryDTO(String name, String capital, String region, long population, String flag) {
+        this.name = name;
+        this.capital = capital;
+        this.region = region;
+        this.population = population;
+        this.flag = flag;
+    }
+
+    public CountryDTO() {}
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCapital() { return capital; }
+    public void setCapital(String capital) { this.capital = capital; }
+
+    public String getRegion() { return region; }
+    public void setRegion(String region) { this.region = region; }
+
+    public long getPopulation() { return population; }
+    public void setPopulation(long population) { this.population = population; }
+
+    public String getFlag() { return flag; }
+    public void setFlag(String flag) { this.flag = flag; }
 }
